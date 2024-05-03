6b0320e78a8d93b3d21bb3719c9649bc9b4be497
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-05-03 19:22:03 +0400

    created burger click toggle

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import data from "../../starter-code/data.json";
+import Burger from "../../public/assets/icon-hamburger.svg";
 
 interface Planet {
   [x: string]: any;
   name: string;
 }
 
+interface ContainerProps {
+  isOpen: boolean;
+}
+
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   const planet: Planet[] = data;
+  const handleBurgerClick = () => {
+    setIsOpen(!isOpen);
+    console.log(!isOpen);
+  };
   return (
-    <Container>
-      <span className="the-planets">THE PLANETS</span>
+    <Container isOpen={isOpen}>
+      <div className="header-box">
+        <span className="the-planets">THE PLANETS</span>
+        <img src={Burger} alt="" onClick={handleBurgerClick} />
+      </div>
       <nav>
         <ul>
           {planet.map((planet, index) => (
-            <li key={index}>{planet.name}</li>
+            <div className="li-box">
+              <div className="circles"></div>
+              <li key={index}>{planet.name}</li>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="6"
+                height="10"
+                viewBox="0 0 6 10"
+                fill="none"
+              >
+                <path opacity="0.4" d="M1 1L5 5L1 9" stroke="white" />
+              </svg>
+            </div>
           ))}
         </ul>
       </nav>
@@ -23,13 +49,15 @@ export default function Header() {
   );
 }
 
-const Container = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 1.37rem 2.56rem;
-  border-bottom: 0.0625rem solid rgba(255, 255, 255, 0.2);
-
+const Container = styled.div<ContainerProps>`
+  .header-box {
+    display: flex;
+    width: 100%;
+    align-items: center;
+    justify-content: space-between;
+    padding: 16px 24px;
+    border-bottom: 0.0625rem solid rgba(255, 255, 255, 0.2);
+  }
   .the-planets {
     color: #fff;
     font-size: 1.75rem;
@@ -38,26 +66,48 @@ const Container = styled.div`
     line-height: normal;
     letter-spacing: -0.06563rem;
     text-transform: uppercase;
-    font-family: "Antonio", sans-serif;
   }
 
   ul {
-    display: flex;
+    position: absolute;
+    top: calc(4.31rem + 1.44rem);
+    right: 0;
+    width: 100%;
+    display: ${(props) => (props.isOpen ? "none" : "flex")};
     justify-content: center;
-    flex-direction: row;
-    gap: 2.06rem;
+    background-color: transparent;
+    flex-direction: column;
+    align-items: center;
     list-style-type: none;
+    padding: 0 1.5rem;
 
-    li {
-      color: #fff;
-      font-family: Spartan, sans-serif;
-      font-size: 0.6875rem;
-      font-style: normal;
-      font-weight: 700;
-      line-height: 227.273%;
-      letter-spacing: 0.0625rem;
-      text-transform: uppercase;
-      cursor: pointer;
+    .li-box {
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      border-bottom: 0.0625rem solid rgba(255, 255, 255, 0.1);
+
+      .circles {
+        width: 1.5rem;
+        height: 1.5rem;
+        border-radius: 50%;
+      }
+
+      li {
+        padding: 1.44rem 0 1.44rem;
+        color: #fff;
+        text-align: center;
+        font-size: 0.9375rem;
+        font-style: normal;
+        font-weight: 700;
+        line-height: 166.667%;
+        letter-spacing: 0.08525rem;
+        text-transform: uppercase;
+      }
+
+      svg {
+      }
     }
   }
 `;
--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,17 +1,18 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Antonio:wght@100..700&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&display=swap');
 *{
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  font-family: "Antonio", "League Spartan", sans-serif;
-  
+  font-family: "League Spartan", sans-serif;
 }
 body{
   background: #070724;
+  background-image: url(/assets/background-stars.svg);
+  background-position: center;
+  background-size: cover;
 }
 `;
 export default GlobalStyles;
