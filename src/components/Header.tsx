237e8ef15350be342cae1e877575fd103cf27373
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-05-11 12:42:14 +0400

    fix: update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import styled from "styled-components";
 import data from "../../starter-code/data.json";
 import Burger from "../../public/assets/icon-hamburger.svg";
